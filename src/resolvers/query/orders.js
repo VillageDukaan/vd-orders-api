7660ab5dc9bec961759d4990c0cfcc50cebd2416
Roger Rajaratnam <roger@sourcier.uk>
2019-02-01 15:48:02 +0000

    Fix typo

--- src/resolvers/query/orders.js
+++ src/resolvers/query/orders.js
@@ -4,7 +4,7 @@ export const getOrders = async (parent, args, context) => {
   try {
     const { database } = context;
     const { limit } = args;
-    const orders = await database.query("SELECT * FROM orderss LIMIT $1", [
+    const orders = await database.query("SELECT * FROM orders LIMIT $1", [
       limit
     ]);
     return orders.rows.map(order => ({
